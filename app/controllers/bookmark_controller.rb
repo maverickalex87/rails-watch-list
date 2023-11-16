bb01be2c75aee032ac55fefa48f399eb3344f83a
alexlupu <alexcristi26@yahoo.com>
2023-11-16 12:27:14 +0100

    watch-list

---
+++ app/controllers/bookmark_controller.rb
@@ -0,0 +1,4 @@
+class BookmarkController < ApplicationController
+  def comment
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'list/name'
+  get 'movies/overview'
+  get 'movies/poster_url'
+  get 'movies/rating'
+  get 'bookmark/comment'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
